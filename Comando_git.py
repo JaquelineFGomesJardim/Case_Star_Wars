c6e32af48efb0e248f5bff4af340afa762bf24c8
Jaqueline Jardim <jacky.f.santos18@gmail.com>
2025-02-04 18:21:18 +0000

    inclusão dos comandos git

--- Comando_git.py
+++ Comando_git.py
@@ -1,2 +1,7 @@
 print('Conseguiu entrar no git')
 
+git init . = inicia um repositorio git
+git add = envia o arquivo para ser comitado
+git commit = comita o arquivo
+git status = verifica o status do repositorio
+git log = mostra todos os comites que foram feitos com as mensagens e quem fez
